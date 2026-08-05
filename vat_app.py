f6ae8ed53ae9fd257dc9acc1c23e783cfd2838e9
404-10-phornsuda <54301@yupparaj.ac.th>
2026-08-05 16:12:39 +0700

    Update vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title("🛒 แอปพลิเคชันคำนวณราคาสินค้ารวม VAT 7%")
-# รับราคาสินค้า
 price = st.number_input("กรอกราคาสินค้า (บาท):", min_value=0.0, value=0.0)
 vat = price * 0.07
 net_price = price + vat
 st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): {vat:.2f} บาท")
 st.header(f"• ราคาสุทธิรวม VAT: {net_price:.2f} บาท")
+st.divider()
 st.write("นางสาวพรสุดา ทมิฬทร เลขที่ 10 ม.4/4")
